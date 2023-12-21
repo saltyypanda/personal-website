1208cf7133a132e95debfcfca7ab39a62bbca744
saltyypanda <esh7943@rit.edu>
2023-12-20 19:37:20 -0500

    Added basic routing placeholders

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import NavBar from './components/Navbar';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
+import Home from './pages/Home';
+// import About from "./pages/about";
+// import Blogs from "./pages/blogs";
+// import SignUp from "./pages/signup";
+// import Contact from "./pages/contact";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                {/* <Route path="/about" element={<About />} />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route
+                    path="/sign-up"
+                    element={<SignUp />}
+                /> */}
+            </Routes>
+        </Router>
+    );
 }
-
+ 
 export default App;
