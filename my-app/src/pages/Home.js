3d551ff07e06c30ae44b0a86192a0d8e5a818e73
saltyypanda <esh7943@g.rit.edu>
2024-01-11 23:41:22 -0500

    Added home screen with some basic text and image

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,10 +3,21 @@ import '../styles.scss'
 
 function Home() {
     return (
-        <div>
-            <h1 className="text-white">Home</h1>
-            <p className="text-white">unimplemented page</p>
-        </div>
+        <>
+            <div className="home-image-container">
+                <div className="home-text">
+                    <p className="code-text">hello world, i'm</p>
+                    <h1 className="name display-1">Tess Hacker</h1>
+                    <p className="mt-4 lead">A second-year software engineering major at the Rochester Institute of Technology</p>
+                </div>
+                <div className="home-images">
+                    <img
+                        src="https://img2.wikia.nocookie.net/__cb20120830141031/adventuretimewithfinnandjake/images/4/4b/Princess_bubblegum_character.png"
+                        alt="princess bubblegum!"
+                    />
+                </div>
+            </div>
+        </>
     );
 }
 
