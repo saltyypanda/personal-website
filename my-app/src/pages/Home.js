a531b0af36b71cb59637666b7886991f38e2b940
saltyypanda <esh7943@g.rit.edu>
2024-01-12 13:34:42 -0500

    Added significantly better typing animation using a package

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import '../styles.scss'
+import TypewriterComponent from "typewriter-effect";
+import Typewriter from 'typewriter-effect/dist/core';
+
+// var introduction = `
+//     I am second-year software engineering major at the Rochester Institute of Technology.
+//     I am an active member of RIT's Society of Software Engineers, or the SSE. I was the SSE's
+//     Head of Talks for Fall 2023 semester, and I am Tech Head for Spring 2024 semester.
+// `
+
+var introduction = `
+    Second-year software engineering major at the Rochester Institute of Technology.
+`
 
 function Home() {
+    const codeTextRef = useRef(null);
+
+    useEffect(() => {
+        const codeTextElement = codeTextRef.current;
+
+        var typewriter = new Typewriter(codeTextElement, {
+            loop: true,
+            delay: 75,
+          });
+          
+          typewriter
+            .pauseFor(2500)
+            .typeString("hello world, i'm")
+            .pauseFor(4000)
+            .deleteChars(10)
+            .pauseFor(500)
+            .start();
+
+        const handleAnimationEnd = () => {
+            console.log("ended")
+        };
+
+        // Add event listener for animationend
+        codeTextElement.addEventListener('animationend', handleAnimationEnd);
+
+        // Cleanup the event listener when the component is unmounted
+        return () => { 
+            codeTextElement.removeEventListener('animationend', handleAnimationEnd);
+        };
+    }, []); // Empty dependency array ensures the effect runs only once on mount
+
     return (
         <>
             <div className="home-image-container">
                 <div className="home-text">
                     <div className="d-inline">
-                        <p className="code-text">hello world, i'm</p>
+                        <p className="code-text" ref={codeTextRef}></p>
                     </div>
                     <h1 className="name display-1">Tess Hacker</h1>
-                    <p className="mt-4 lead">A second-year software engineering major at the Rochester Institute of Technology</p>
+                    <p className="mt-4 lead text-white centered">{introduction}</p>
                 </div>
                 <div className="home-images">
                     <img
@@ -19,6 +62,9 @@ function Home() {
                     />
                 </div>
             </div>
+            {/* <div className="d-flex flex-col text-center justify-content-center w-50 mx-auto">
+                <p className="mt-4 lead text-white centered">{introduction}</p>
+            </div> */}
         </>
     );
 }
